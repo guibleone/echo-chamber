a561b30dfbe8e2004864a85aab79fc277ea728a7
Guilherme Bastos Leone <guilhermebastosleone@gmail.com>
2024-01-24 11:56:05 -0300

    Remove sensitive information from .env file and update layout styling

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -3,5 +3,5 @@ export default function AuthLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <main className="flex justify-center items-center min-h-screen">{children}</main>;
+  return <main className="flex justify-center px-4 items-center min-h-screen pb-[150px]">{children}</main>;
 }
